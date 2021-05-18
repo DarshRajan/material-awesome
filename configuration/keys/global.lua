018f2766428c0c8f2851f8bab7128e1db298b857
Darsh Rajan <darsh.rajan95@gmail.com>
2021-05-18 19:55:08 +0530

    Improve window switching

--- configuration/keys/global.lua
+++ configuration/keys/global.lua
@@ -14,8 +14,9 @@ local globalKeys =
   -- Tag browsing
   awful.key({modkey}, 'Up', awful.tag.viewprev, {description = 'view previous', group = 'tag'}),
   awful.key({modkey}, 'Down', awful.tag.viewnext, {description = 'view next', group = 'tag'}),
-  --awful.key({altkey, 'Control'}, 'Up', awful.tag.viewprev, {description = 'view previous', group = 'tag'}),
-  --awful.key({altkey, 'Control'}, 'Down', awful.tag.viewnext, {description = 'view next', group = 'tag'}),
+  -- some vim shortcuts
+  awful.key({modkey}, 'k', awful.tag.viewprev, {description = 'view previous', group = 'tag'}),
+  awful.key({modkey}, 'j', awful.tag.viewnext, {description = 'view next', group = 'tag'}),
   awful.key({modkey}, 'Escape', awful.tag.history.restore, {description = 'go back', group = 'tag'}),
   -- Default client focus
   awful.key(
